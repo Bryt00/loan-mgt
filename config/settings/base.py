4dd5d6a7c2367044cd219a6e57aa0f750d4f68b5
Bryt00 <mrbright63@gmail.com>
2026-08-07 11:49:47 +0000

    first commit

--- config/settings/base.py
+++ config/settings/base.py
@@ -20,7 +20,6 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 # Application definition
 INSTALLED_APPS = [
     'jazzmin',
-    'tailwind',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
